4918cba21d143a3123c839ba4b3ba9b1f03bb190
Yannick Brosseau <yannick.brosseau@gmail.com>
2026-02-06 09:00:07 -0500

    Add function to predict car ownership
    Based on a model generated from classifying OD data and other models. Resulting model is in the file localisation/models/xgb_car_ownership_model.onnx
    
    We add the onnxruntime-node module to be able to run inference from that model. We needed to add a few entries
    in jest.config.ts to let Jest properly handle the data format used by the model. We also need to add the --runInBand
    option to the jest runner so that all test run sequentially, there's a issue with onnx and jest is seems. #101 for follow ups
    
    There's still a few TODO to do in the code, including fetching actual proximity indexes, but this give the general
    architecture, so we can start with the integration. Tested with unit tests and in the actually application

---
+++ localisation/src/survey/calculations/carownership.ts
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2026, Polytechnique Montreal and contributors
+ *
+ * This file is licensed under the MIT License.
+ * License text available at https://opensource.org/licenses/MIT
+ */
+
+import * as ort from 'onnxruntime-node';
+import path from 'path';
+
+const MODEL_FILENAME = path.resolve(__dirname, '../../../models/xgb_car_ownership_model.onnx');
+
+// Load session as a Singleton so that we don't have to lead it each time we run the function
+let session: ort.InferenceSession | null = null;
+
+async function getSession(): Promise<ort.InferenceSession> {
+    if (!session) {
+        session = await ort.InferenceSession.create(MODEL_FILENAME);
+    }
+    return session;
+}
+
+interface ProximityIndexes {
+    idx_prox_emp: ort.Tensor;
+    idx_prox_pharma: ort.Tensor;
+    idx_prox_garderie: ort.Tensor;
+    idx_prox_sante: ort.Tensor;
+    idx_prox_epicerie: ort.Tensor;
+    idx_prox_educpri: ort.Tensor;
+    idx_prox_educsec: ort.Tensor;
+    idx_prox_bibl: ort.Tensor;
+    idx_prox_parcs: ort.Tensor;
+    idx_prox_transit: ort.Tensor;
+}
+
+async function getProximityIndexes(geography: GeoJSON.Feature<GeoJSON.Point>): Promise<ProximityIndexes> {
+    // TODO: Implement actual proximity lookup based on coordinates
+    // const [lng, lat] = geography.geometry.coordinates;
+
+    const tensor = (value: number) => new ort.Tensor('float32', Float32Array.from([value]), [1, 1]);
+
+    return {
+        idx_prox_emp: tensor(1.0),
+        idx_prox_pharma: tensor(1.0),
+        idx_prox_garderie: tensor(1.0),
+        idx_prox_sante: tensor(1.0),
+        idx_prox_epicerie: tensor(1.0),
+        idx_prox_educpri: tensor(1.0),
+        idx_prox_educsec: tensor(1.0),
+        idx_prox_bibl: tensor(1.0),
+        idx_prox_parcs: tensor(1.0),
+        idx_prox_transit: tensor(1.0)
+    };
+}
+
+export async function predictCarOwnership(data: {
+    geography: GeoJSON.Feature<GeoJSON.Point>;
+    householdSize: number;
+    numberPermits: number;
+    income: number;
+}): Promise<number> {
+    // Fetch proximity indexes
+    const proximityIndexes = await getProximityIndexes(data.geography);
+
+    // TODO
+    // Map income to income range of the model
+    // This is what was used for the model:
+    //     income_mapping = {
+    //    "Less than $10,000": 1,
+    //    "$10,000 to $19,999": 1,
+    //    "$20,000 to $29,999": 1,
+    //    "$30,000 to $39,999": 2,
+    //    "$40,000 to $49,999": 2,
+    //    "$50,000 to $59,999": 2,
+    //    "$60,000 to $69,999": 3,
+    //    "$70,000 to $79,999": 3,
+    //    "$80,000 to $89,999": 3,
+    //    "$90,000 to $99,999": 4,
+    //    "$100,000 to $149,999": 5,
+    //    "$150,000 to $199,999": 6,
+    //    "$200,000 and more": 8,
+    //    "I don't know": 10,
+    //    "I prefer not to answer": 9,
+    //}
+    const incomeLevel = 2;
+
+    // TODO ensure that numberPermits is not higher than householdSize
+
+    const session = await getSession();
+
+    const inputs: Record<string, ort.Tensor> = {
+        perslogi: new ort.Tensor('float32', Float32Array.from([data.householdSize]), [1, 1]),
+        nbPermis: new ort.Tensor('float32', Float32Array.from([data.numberPermits]), [1, 1]),
+        revenu: new ort.Tensor('string', [String(incomeLevel)], [1, 1]),
+        ...proximityIndexes
+    };
+
+    const results = await session.run(inputs);
+
+    if (!results.label?.data?.length) {
+        throw new Error('Car Ownership Model returned invalid label');
+    }
+
+    const carPrediction = Number(results.label.data[0]);
+    if (Number.isNaN(carPrediction)) {
+        throw new Error(`Invalid car prediction value: ${results.label.data[0]}`);
+    }
+
+    return carPrediction;
+}
